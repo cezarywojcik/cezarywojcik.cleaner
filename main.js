b05f0d269beb225bc11d1d3fa084fe8f3de065a6
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-07-08 14:52:26 -0700

    replace tabs with spaces

--- main.js
+++ main.js
@@ -7,28 +7,47 @@ define(function (require, exports, module) {
   var CommandManager  = brackets.getModule("command/CommandManager"),
       Menus           = brackets.getModule("command/Menus"),
       EditorManager   = brackets.getModule("editor/EditorManager"),
+      Editor          = brackets.getModule("editor/Editor").Editor,
       DocumentManager = brackets.getModule("document/DocumentManager");
 
   function clean() {
-    var txt = DocumentManager.getCurrentDocument().getText();
+    // get doc and editor
+    var CurrentEditor = EditorManager.getCurrentFullEditor();
+    var Document = DocumentManager.getCurrentDocument();
+
+    // get document content
+    var txt = Document.getText();
+
     // remove trailing whitespace
     txt = txt.replace(/[^\S\n]+$/gm, "");
-    // add newline if there is no newline
+
+    // replace any tabs with spaces
+    if (!Editor.getUseTabChar()) {
+      var tabSize = Editor.getSpaceUnits();
+      var spaces = new Array(tabSize+1).join(" ");
+      txt = txt.replace(/\t/gm, spaces);
+    }
+
+    // add newline at end of file if there is no newline
     var lastChar = txt.charAt(txt.length-1);
     if (lastChar !== "\n") {
       txt += "\n";
     }
+
     // get cursor position
-    var cursorPos = EditorManager.getCurrentFullEditor().getCursorPos();
+    var cursorPos = CurrentEditor.getCursorPos();
+
     // add cleaned text
-    DocumentManager.getCurrentDocument().setText(txt);
+    Document.setText(txt);
+
     // apply cursor position
-    EditorManager.getCurrentFullEditor().setCursorPos(cursorPos);
+    CurrentEditor.setCursorPos(cursorPos);
   }
 
   var FORMAT_ID = "cezarywojcik.clean";
   CommandManager.register("Clean", FORMAT_ID, clean);
 
   var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+  menu.addMenuDivider();
   menu.addMenuItem(FORMAT_ID, "Ctrl-Alt-C");
 });
